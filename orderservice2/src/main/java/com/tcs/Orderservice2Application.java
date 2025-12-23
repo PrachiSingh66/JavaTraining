4a88f0a28da26c11d1aeeb73015cddb210821300
PrachiSingh66 <124974283+PrachiSingh66@users.noreply.github.com>
2025-12-23 09:37:18 +0000

    order service2

---
+++ orderservice2/src/main/java/com/tcs/Orderservice2Application.java
@@ -0,0 +1,15 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(basePackages="com.tcs.client")
+@SpringBootApplication
+public class Orderservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Orderservice2Application.class, args);
+	}
+
+}
